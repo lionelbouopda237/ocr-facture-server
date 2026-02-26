81c8b3a462fae45aa9df30b4b25e2a5cbad66a2e
lionelbouopda237 <bdlionel91@gmail.com>
2026-02-26 08:28:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import pytesseract
 from PIL import Image
 import io
+import cv2
+import numpy as np
 
 app = FastAPI()
 
@@ -15,14 +17,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ---- OCR Endpoint ----
 @app.post("/ocr")
 async def ocr_invoice(file: UploadFile = File(...)):
     try:
         contents = await file.read()
-        image = Image.open(io.BytesIO(contents))
 
-        text = pytesseract.image_to_string(image)
+        # Convertir en image OpenCV
+        np_arr = np.frombuffer(contents, np.uint8)
+        image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        # Prétraitement
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+        thresh = cv2.threshold(gray, 0, 255,
+                               cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+        # OCR
+        text = pytesseract.image_to_string(thresh, lang="eng")
 
         return {
             "success": True,
